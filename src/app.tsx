e3789e8b0f1a926244062bbd56b91df9f1fdf8db
André Silva <andreoew@hotmail.com>
2024-02-06 19:33:18 -0400

    update

--- src/app.tsx
+++ src/app.tsx
@@ -2,17 +2,18 @@ import logo from "./assets/logo-nlw-expert.svg"
 import { NewNoteCard } from "./components/new-note-card"
 import { NoteCard } from "./components/note-card"
 
+
 export function App() {
 
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW expert" />
-      
+
       <form className="w-full ">
-        <input 
-          type="text" 
-          placeholder="Busque em suas notas..." 
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
         />
       </form>
@@ -21,13 +22,13 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
+
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello Wold'
+        }} />
         
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+
       </div>
     </div>
 
